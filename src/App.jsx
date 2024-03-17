ee0a70fc9d6a547c4b5b66e16ab678f440d7498d
CyprianESPI <cyprianespinoza@gmail.com>
2024-03-17 08:53:48 +0100

    Add dark/light them switch

--- src/App.jsx
+++ src/App.jsx
@@ -16,55 +16,59 @@ function App() {
     case APP_MODES[0]:
       return (
         <>
-          <h1>CV App</h1>
-          <label htmlFor="mode-toggle">{mode}</label>
-          <input type="checkbox" id="mode-toggle" onChange={(e) => {
-            // We cast to number to conver false/true to 0/1
-            setMode(APP_MODES[Number(e.target.checked)]);
-          }} />
-          <label htmlFor="theme-toggle">{theme}</label>
-          <input type="checkbox" id="theme-toggle" onChange={(e) => {
-            // We cast to number to conver false/true to 0/1
-            setTheme(APP_THEMES[Number(e.target.checked)]);
-          }} />
-          <main>
-            <section className="card">
-              <h2>General</h2>
-              <Contact name={name} setName={setName} email={email} setEmail={setEmail} tel={tel} setTel={setTel} ></Contact>
-            </section>
-            <section className="card">
-              <h2>Skills</h2>
-            </section>
-            <section className="card">
-              <h2>Work Experience</h2>
-            </section>
-            <section className="card">
-              <h2>Education</h2>
-            </section>
-          </main>
+          <div style={{ colorScheme: theme }}>
+            <h1>CV App</h1>
+            <label htmlFor="mode-toggle">{mode}</label>
+            <input type="checkbox" id="mode-toggle" onChange={(e) => {
+              // We cast to number to conver false/true to 0/1
+              setMode(APP_MODES[Number(e.target.checked)]);
+            }} />
+            <label htmlFor="theme-toggle">{theme}</label>
+            <input type="checkbox" id="theme-toggle" onChange={(e) => {
+              // We cast to number to conver false/true to 0/1
+              setTheme(APP_THEMES[Number(e.target.checked)]);
+            }} />
+            <main>
+              <section className="card">
+                <h2>General</h2>
+                <Contact name={name} setName={setName} email={email} setEmail={setEmail} tel={tel} setTel={setTel} ></Contact>
+              </section>
+              <section className="card">
+                <h2>Skills</h2>
+              </section>
+              <section className="card">
+                <h2>Work Experience</h2>
+              </section>
+              <section className="card">
+                <h2>Education</h2>
+              </section>
+            </main>
+          </div>
         </>
       )
     case APP_MODES[1]:
       return (
         <>
-          <h1>CV App</h1>
-          <label htmlFor="mode-toggle">{mode}</label>
-          <input type="checkbox" id="mode-toggle" onChange={(e) => {
-            // We cast to number to conver false/true to 0/1
-            setMode(APP_MODES[Number(e.target.checked)]);
-          }} />
-          <label htmlFor="theme-toggle">{theme}</label>
-          <input type="checkbox" id="theme-toggle" onChange={(e) => {
-            // We cast to number to conver false/true to 0/1
-            setTheme(APP_THEMES[Number(e.target.checked)]);
-          }} />
-          <main>
-            <section className="card">
-              <h2>General</h2>
-              <h3>Name</h3>
-              <p>{name}</p>
-            </section>
-          </main>
+          <div style={{ colorScheme: theme }}>
+            <h1>CV App</h1>
+            <label htmlFor="mode-toggle">{mode}</label>
+            <input type="checkbox" id="mode-toggle" onChange={(e) => {
+              // We cast to number to conver false/true to 0/1
+              setMode(APP_MODES[Number(e.target.checked)]);
+            }} />
+            <label htmlFor="theme-toggle">{theme}</label>
+            <input type="checkbox" id="theme-toggle" onChange={(e) => {
+              // We cast to number to conver false/true to 0/1
+              setTheme(APP_THEMES[Number(e.target.checked)]);
+            }} />
+            <main>
+              <section className="card">
+                <h2>General</h2>
+                <h3>Name</h3>
+                <p>{name}</p>
+              </section>
+            </main>
+          </div>
         </>
       )
 
