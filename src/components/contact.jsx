3abc04bea470d71e8f2df9e4165e2692b8ef00eb
CyprianESPI <cyprianespinoza@gmail.com>
2024-03-17 07:12:55 +0100

    Add Contact Input component

---
+++ src/components/contact.jsx
@@ -0,0 +1,12 @@
+function Contact() {
+    return <div>
+        <label for="name">Name</label>
+        <input id="name" type="text" />
+        <label for="email">Email</label>
+        <input id="email" type="email" />
+        <label for="tel">Phone Number</label>
+        <input id="tel" type="tel" />
+    </div>;
+}
+
+export default Contact;
