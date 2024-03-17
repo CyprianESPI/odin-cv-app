795151ee7546bc4defd09e97fb0143243375e841
CyprianESPI <cyprianespinoza@gmail.com>
2024-03-17 07:30:13 +0100

    Update Contact for name state

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -1,10 +1,15 @@
+import { useState } from "react";
+
 function Contact() {
+    const [name, setName] = useState("");
+    console.log("Contact:", this);
+
     return <div>
-        <label for="name">Name</label>
-        <input id="name" type="text" />
-        <label for="email">Email</label>
+        <label htmlFor="name">Name</label>
+        <input id="name" type="text" value={name} onChange={(e) => setName(e.target.value)} placeholder="John Smith" />
+        <label htmlFor="email">Email</label>
         <input id="email" type="email" />
-        <label for="tel">Phone Number</label>
+        <label htmlFor="tel">Phone Number</label>
         <input id="tel" type="tel" />
     </div>;
 }
