d36d3e7f198af82e452299f4aeeeda42b2c3302d
CyprianESPI <cyprianespinoza@gmail.com>
2024-03-17 07:13:46 +0100

    Clean-up App and use Contact component

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,28 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import Contact from './components/contact'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
       <h1>CV App</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <main>
+        <section className="card">
+          <h2>General</h2>
+          <Contact></Contact>
+        </section>
+        <section className="card">
+          <h2>Skills</h2>
+          <Contact></Contact>
+        </section>
+        <section className="card">
+          <h2>Work Experience</h2>
+          <Contact></Contact>
+        </section>
+        <section className="card">
+          <h2>Education</h2>
+          <Contact></Contact>
+        </section>
+      </main>
     </>
   )
 }
