08c17ac0c53f47bd2aabf193bbec3a6181f3accf
CyprianESPI <cyprianespinoza@gmail.com>
2024-06-23 13:33:58 +0200

    Update WorkXp Role

--- src/components/work-xp.jsx
+++ src/components/work-xp.jsx
@@ -4,12 +4,18 @@ import { APP_MODES } from "../App";
 function WorkXpCmp({mode, xp }) {
     const [workXp, setWorkXp] = useState({...xp});
 
-
     switch(mode) {
         case APP_MODES[0]:
             return <section>
                 <h3>{workXp.role}</h3>
-                <div style={{display:"flex", justifyContent:"space-around", flexWrap:"wrap"}}>
+                <div style={{display:"flex", flexDirection:"column", flexWrap:"wrap"}}>
+                    <div>
+                    <label>Role</label>
+                    <input type="text" value={workXp.role ?? ""}
+                        onChange={e => setWorkXp({...workXp, role: e.target.value})}
+                        placeholder="Google" />
+                    </div>
+                
                 </div>
             </section>;
         case APP_MODES[1]:
