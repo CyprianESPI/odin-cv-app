ca93efb3a7c9972e6b7bebb77c0c9cf9f1fe0b6e
CyprianESPI <cyprianespinoza@gmail.com>
2024-06-23 13:37:30 +0200

    Update work xp cmp

--- src/components/work-xp.jsx
+++ src/components/work-xp.jsx
@@ -10,12 +10,35 @@ function WorkXpCmp({mode, xp }) {
                 <h3>{workXp.role}</h3>
                 <div style={{display:"flex", flexDirection:"column", flexWrap:"wrap"}}>
                     <div>
-                    <label>Role</label>
-                    <input type="text" value={workXp.role ?? ""}
-                        onChange={e => setWorkXp({...workXp, role: e.target.value})}
-                        placeholder="Google" />
+                        <label>Company</label>
+                        <input type="text" value={workXp.company ?? ""}
+                            onChange={e => setWorkXp({...workXp, company: e.target.value})}
+                            placeholder="Google" />
                     </div>
-                
+                    <div>
+                        <label>Role</label>
+                        <input type="text" value={workXp.role ?? ""}
+                            onChange={e => setWorkXp({...workXp, role: e.target.value})}
+                            placeholder="Sw Engineer" />
+                    </div>
+                    <div>
+                        <label>Role Description</label>
+                        <input type="text" value={workXp.roleDescription ?? ""}
+                            onChange={e => setWorkXp({...workXp, roleDescription: e.target.value})}
+                            placeholder="Increased profits by 500%" />
+                    </div>
+                    <div>
+                        <label>From</label>
+                        <input type="text" value={workXp.start ?? ""}
+                            onChange={e => setWorkXp({...workXp, start: e.target.value})}
+                            placeholder="2012" />
+                    </div>
+                    <div>
+                        <label>To</label>
+                        <input type="text" value={workXp.end ?? ""}
+                            onChange={e => setWorkXp({...workXp, end: e.target.value})}
+                            placeholder="Now" />
+                    </div>
                 </div>
             </section>;
         case APP_MODES[1]:
